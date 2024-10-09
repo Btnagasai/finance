43f31f369759e1b06741bb9d27a4628860258ba0
Tulasi Naga Sai <btnagasai@gmail.com>
2024-10-09 14:44:38 +0530

    shadow

--- src/Finance2.jsx
+++ src/Finance2.jsx
@@ -1,15 +1,15 @@
 function Finance2(){
     return(
         <div className="grid   gap-4 bg-[#f8f5f0] items-center m-auto h-screen justify-center">
-            <div className="grid grid-cols-4 grid-rows-6  gap-4">
-            <div className="row-span-6 bg-black p-[100px]"></div>
-            <div className="col-span bg-black p-10"></div>
-            <div className="col-span bg-white p-10"></div>
-            <div className="col-span bg-white p-10"></div>
-            <div className="col-span-2 row-span-2 bg-white p-10"></div>
-            <div className="row-span-3 bg-white p-10"></div>
-            <div className="col-span-2 row-span-4 bg-white p-10"></div>
-            <div className="row-span-3 bg-white p-10"></div>
+            <div className="grid grid-cols-4 grid-rows-6 shadow-2xl shadow-black  gap-4">
+            <div className="row-span-6 bg-black p-[100px] shadow-2xl shadow-black"></div>
+            <div className="col-span bg-black shadow-2xl shadow-black p-10"></div>
+            <div className="col-span bg-white shadow-2xl shadow-black p-10"></div>
+            <div className="col-span bg-white shadow-2xl shadow-black p-10"></div>
+            <div className="col-span-2 row-span-2 shadow-2xl shadow-black bg-white p-10"></div>
+            <div className="row-span-3 bg-white shadow-2xl shadow-black p-10"></div>
+            <div className="col-span-2 row-span-4 shadow-2xl shadow-black bg-white p-10"></div>
+            <div className="row-span-3 bg-white shadow-2xl shadow-black p-10"></div>
 </div>
         </div>
     )
