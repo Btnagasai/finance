4c18871dab815e9b9e1833994d89e474433ff92f
Tulasi Naga Sai <btnagasai@gmail.com>
2024-10-09 14:23:15 +0530

    changes

--- src/Finance.jsx
+++ src/Finance.jsx
@@ -5,7 +5,7 @@ return(
         <div className="grid  justify-center items-center h-screen max-w-full m-auto  bg-[#f8f5f0]">
             
             <div className="grid grid-cols-8  grid-rows-4 gap-6 shadow-2xl shadow-black p-5">
-                <div className="row-span-12 rounded-e-3xl bg-black shadow-2xl shadow-black  text-white text-[30px] pl-5 pt-5 font-bold font-public-sans  "> finance</div>
+                <div className="row-span-10 rounded-e-3xl bg-black shadow-2xl shadow-black  text-white text-[30px] pl-5 pt-5 font-bold font-public-sans  "> finance</div>
                 <div className="col-span-6 rounded-xl text-[32px] font-bold font-public-sans pt-5  mt-16">Overview</div>
                 <div className="col-span-2 rounded-xl shadow-2xl shadow-black p-[50px] bg-black"></div>
                 <div className="col-span-2 rounded-xl shadow-2xl shadow-black  bg-white"></div>
@@ -20,8 +20,8 @@ return(
                 
                 </div>
                 <div className="col-span-3 rounded-xl row-span-2 shadow-2xl shadow-black bg-white"></div>
-                <div className="col-span-3  rounded-xl row-span-9 shadow-2xl shadow-black bg-white"></div>
-                <div className="col-span-3 rounded-xl row-span-8 shadow-2xl shadow-black bg-white"></div>
+                <div className="col-span-3  rounded-xl row-span-7 shadow-2xl shadow-black bg-white"></div>
+                <div className="col-span-3 rounded-xl row-span-6 shadow-2xl shadow-black bg-white"></div>
              
                </div> 
             </div>
