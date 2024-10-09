8c5ee0dbf2e315dd67f1ea75fae678ad9012d2b8
Tulasi Naga Sai <btnagasai@gmail.com>
2024-10-09 14:42:13 +0530

    finance2

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Finance from './Finance'
+import Finance2 from './Finance2'
 
 function App() {
  
 
   return (
    <div>
-      <Finance/>
+      {/* <Finance/> */}
+      <Finance2/>
    </div>
   )
 }
---
+++ src/Finance2.jsx
@@ -0,0 +1,17 @@
+function Finance2(){
+    return(
+        <div className="grid   gap-4 bg-[#f8f5f0] items-center m-auto h-screen justify-center">
+            <div className="grid grid-cols-4 grid-rows-6  gap-4">
+            <div className="row-span-6 bg-black p-[100px]"></div>
+            <div className="col-span bg-black p-10"></div>
+            <div className="col-span bg-white p-10"></div>
+            <div className="col-span bg-white p-10"></div>
+            <div className="col-span-2 row-span-2 bg-white p-10"></div>
+            <div className="row-span-3 bg-white p-10"></div>
+            <div className="col-span-2 row-span-4 bg-white p-10"></div>
+            <div className="row-span-3 bg-white p-10"></div>
+</div>
+        </div>
+    )
+}
+export default Finance2
