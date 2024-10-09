fc7f532da57966c14679e665df9eec779a01c704
Tulasi Naga Sai <btnagasai@gmail.com>
2024-10-09 14:54:51 +0530

    fontchanges

--- src/Finance2.jsx
+++ src/Finance2.jsx
@@ -1,15 +1,22 @@
 function Finance2(){
     return(
         <div className="grid   gap-4 bg-[#f8f5f0] items-center m-auto h-screen justify-center">
-            <div className="grid grid-cols-4 grid-rows-6 shadow-2xl shadow-black  gap-4">
-            <div className="row-span-6 bg-black p-[100px] shadow-2xl shadow-black"></div>
-            <div className="col-span bg-black shadow-2xl shadow-black p-10"></div>
-            <div className="col-span bg-white shadow-2xl shadow-black p-10"></div>
-            <div className="col-span bg-white shadow-2xl shadow-black p-10"></div>
-            <div className="col-span-2 row-span-2 shadow-2xl shadow-black bg-white p-10"></div>
-            <div className="row-span-3 bg-white shadow-2xl shadow-black p-10"></div>
-            <div className="col-span-2 row-span-4 shadow-2xl shadow-black bg-white p-10"></div>
-            <div className="row-span-3 bg-white shadow-2xl shadow-black p-10"></div>
+            <div className="grid grid-cols-4 grid-rows-6 shadow-2xl shadow-black  gap-4 p-5">
+            <div className="row-span-8 bg-black p-[100px] shadow-2xl rounded-e-3xl shadow-black"></div>
+            <div className="col-span-3 font-public-sans mt-10  text-[30px]">Overview</div>
+            <div className="col-span bg-black shadow-2xl rounded-2xl shadow-black p-10"></div>
+            <div className="col-span bg-white shadow-2xl rounded-2xl shadow-black p-10"></div>
+            <div className="col-span bg-white shadow-2xl rounded-2xl shadow-black p-10"></div>
+            <div className="col-span-2 row-span-2 shadow-2xl rounded-2xl shadow-black bg-white p-10 font-public-sans text-[25px]">Post
+            <span className="grid grid-cols-2 items-center justify-center text-black gap-5 ">
+                        <span className="bg-[#f8f5f0] shadow shadow-black p-2 w-[200px] h-28 ml-14 rounded-2xl"></span>
+                      
+                        <span className=" p-2 w-[200px] h-28 rounded-2xl "></span>
+                    </span>
+            </div>
+            <div className="row-span-3 bg-white shadow-2xl rounded-2xl shadow-black p-10"></div>
+            <div className="col-span-2 row-span-4 shadow-2xl rounded-2xl shadow-black bg-white p-10"></div>
+            <div className="row-span-3 bg-white shadow-2xl rounded-2xl shadow-black p-10"></div>
 </div>
         </div>
     )
